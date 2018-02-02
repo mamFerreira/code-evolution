ea0ad4519aaf70187474f219d1e76af74f732145
mamFerreira <mamfer89@gmail.com>
2018-02-02 17:32:31 +0100

    Estructura completa client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'CODE-EVOLUTION';
+  public title = 'CODE-EVOLUTION';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+    this.user = new User ('','','','','','ROLE_USER','','');
+
+  }
+
 }
